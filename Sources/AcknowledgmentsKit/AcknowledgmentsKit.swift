da2dab3d818f3b97daa7d3e73a6b4ae485552f53
Jiaxin Shou <shoujiaxin@gmail.com>
2024-11-23 00:20:39 +0800

    feat: add initial project

---
+++ Sources/AcknowledgmentsKit/AcknowledgmentsKit.swift
@@ -0,0 +1,6 @@
+//
+//  AcknowledgmentsKit.swift
+//  AcknowledgmentsKit
+//
+//  Created by Jiaxin Shou on 2024/11/23.
+//
---
+++ Tests/AcknowledgmentsKitTests/AcknowledgmentsKitTests.swift
@@ -0,0 +1,14 @@
+//
+//  AcknowledgmentsKitTests.swift
+//  AcknowledgmentsKitTests
+//
+//  Created by Jiaxin Shou on 2024/11/23.
+//
+
+@testable import AcknowledgmentsKit
+import Testing
+
+@Test
+func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
